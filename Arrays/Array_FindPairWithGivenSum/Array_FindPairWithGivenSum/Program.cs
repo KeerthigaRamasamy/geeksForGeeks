a613429a74e4fce5a2bd4a2aac3a4daa4d93105b
Keerthiga Ramasamy <m1037727@mindtree.com>
2017-12-07 18:39:17 +0530

    given an array and a number, check for a pair with the given number as sum

---
+++ Arrays/Array_FindPairWithGivenSum/Array_FindPairWithGivenSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array_FindPairWithGivenSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[6] {1, 4, 45, 6, 10, -8 };
+            int sum = 16;
+            FindPair(arr, sum);
+            Console.ReadKey();
+
+        }
+        public static void FindPair(int[] arr,int sum)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for(int i=0;i<arr.Length;i++)
+            {
+               if(!map.ContainsKey(arr[i]))
+                {
+                    map.Add(arr[i], 1);
+                }
+                int diff = Math.Abs((sum - arr[i]));
+                if(map.ContainsKey(diff))
+                {
+                    Console.WriteLine("The pair is " + diff + " " + arr[i]);
+                    return;
+                }
+            }
+            Console.WriteLine("The array doesn't have numbers that gives the sum");
+        }
+    }
+}
