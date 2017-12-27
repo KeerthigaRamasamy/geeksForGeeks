ca5679f46fe5346e2e37cfd1f08f272bf4fb97ea
Keerthiga Ramasamy <m1037727@mindtree.com>
2017-12-27 19:30:01 +0530

    Knight'sTourProblem

---
+++ Backtracking/Knight'sTourProblem/Knight'sTourProblem/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Knight_sTourProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] sol = new int[8, 8] { {-1,-1,-1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 } };
+            int[] xMove = new int[] {2,1,-1,-2,-2,-1,1,2 };
+            int[] yMove = new int[] {1,2,2,1,-1,-2,-2,-1 };
+            sol[0, 0] = 0;
+            if(KnightTour(sol,0,0,1,8,xMove,yMove))
+            {
+                for(int i=0;i<8;i++)
+                {
+                    for(int j=0;j<8;j++)
+                    {
+                        Console.Write(sol[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static bool isSafe(int[,] sol,int x,int y,int n)
+        {
+            return (x >= 0 && x<n && y>=0 && y < n && sol[x, y] == -1);
+        }
+
+        public static bool KnightTour(int[,] sol,int x,int y,int movei,int n,int[] xMove,int[] yMove)
+        {
+            int nextMoveX, nextMoveY;
+            if (movei==n*n)
+            {
+                return true;
+            }
+            for(int i=0;i<8;i++)
+            {
+                nextMoveX = x + xMove[i];
+                nextMoveY = y + yMove[i];
+                if (isSafe(sol, nextMoveX, nextMoveY, n))
+                {
+                    sol[nextMoveX, nextMoveY] = movei;                    
+                    if (KnightTour(sol, nextMoveX, nextMoveY, movei + 1, n, xMove, yMove))
+                    {
+                        return true;
+                    }
+                    sol[nextMoveX, nextMoveY] = -1;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Recursion/PalindromeNumberCheck'/PalindromeNumberCheck'/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PalindromeNumberCheck_
+{
+    class Program
+    {
+        public static bool isPalindrome = false;
+        public static Stack<int> stack=new Stack<int>();
+        public static int n = 123211;
+        public static int n1 = n;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(n));
+            Console.ReadKey();
+        }
+
+        public static bool IsPalindrome(int n)
+        {
+            if(n!=0)
+            {
+                n = n / 10;
+                IsPalindrome(n);
+            }
+            if(n % 10 == n1 % 10)
+            {
+                n1 = n1 / 10;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
